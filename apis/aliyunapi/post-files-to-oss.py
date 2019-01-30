260131766d1aa7a911c9a48049fbbe315622e2ae
丶老龚 <jaybing926@outlook.com>
2019-01-30 11:00:19 +0800

    Update post-files-to-oss.py

--- apis/aliyunapi/post-files-to-oss.py
+++ apis/aliyunapi/post-files-to-oss.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python
-
+#coding=utf8
 import os
 import oss2
+import sys
+from colorama import init, Fore, Back, Style
+import shutil
 
+init(autoreset=True)
 
-auth = oss2.Auth('xxx','xxx')
-bucket = oss2.Bucket(auth,'oss-cn-beijing.aliyuncs.com','www-idouya-net')
+if len(sys.argv) < 3:
+    print(Fore.RED + "位置参数不够~~")
+else:
+    if sys.argv[1] == "post":
+        print(Fore.RED + "\t执行前请确认：")
+        print(Fore.RED + """\t1.确认OSS的Bucket准确
+            2.确认Bucket的目录路径准确""")
 
-path = 'test'
+        auth = oss2.Auth('xxx','xxx')
+        bucket = oss2.Bucket(auth,'oss-ap-southeast-1.aliyuncs.com','game-sources-myanmargame-net')
 
-for fpath,dirs,fs in os.walk(path):
-    for f in fs:
-	print(f) 
-        file = os.path.join(fpath,f)
-	print(file)
-	file2 = os.path.join('secdir',file)
-	print(file2)
-#        bucket.put_object_from_file(file2,file)
+        init(autoreset=True)
+        path = sys.argv[2]
+        oss_path = 'IOS_Resources'
 
-#file2为OSS上的文件路径名，file为本地文件路径名
+        try:
+            os.rename(path,oss_path)
+            shutil.rmtree(oss_path+'/.svn')
+        except OSError,e:
+            print(Fore.GREEN + ".svn没发现.")
+
+        oss_secdir = ''
+
+        for fpath,dirs,fs in os.walk(oss_path):
+            for f in fs:
+                file = os.path.join(fpath,f)
+                file2 = os.path.join(oss_secdir,file)
+                print(Fore.YELLOW + "本地文件路径：%s , OSS路径：%s" % (file,file2))
+                bucket.put_object_from_file(file2,file)
+        os.rename(oss_path,path)
+    else:
+        print(Fore.RED + "HELP")
+# file2为OSS上的文件路径名，file为本地文件路径名
